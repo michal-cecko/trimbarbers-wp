1e747fa020f182da5ae270914d83d142a20deee8
michalcecko <ceckomichal@gmail.com>
2023-05-03 21:18:35 +0200

    Fix: ICS, bol tam zabudnuty cancel link

--- template_parts/email_template.php
+++ template_parts/email_template.php
@@ -396,7 +396,7 @@
                         <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
 			padding-top: 25px;
 			padding-bottom: 5px;" class="button"><a
-                                    href="<?= $cancelLink ?>" target="_blank" style="text-decoration: none;">
+                                    href="<?= $icsLink ?>" target="_blank" style="text-decoration: none;">
                                 <table border="0" cellpadding="0" cellspacing="0" align="center"
                                        style="max-width: 240px; min-width: 120px; border-collapse: collapse; border-spacing: 0; padding: 0;">
                                     <tr>
